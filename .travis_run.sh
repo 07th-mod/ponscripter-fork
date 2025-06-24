948d3ebaddbf57b17ee364dacd2f3f416531b48c
drojf <1249449+drojf@users.noreply.github.com>
2025-06-24 22:35:31 +1000

    Fix configure failing test compile due to -c argument

--- .travis_run.sh
+++ .travis_run.sh
@@ -76,7 +76,7 @@ else
 	# find src/extlib/src/freetype-2.3.5/builds -type f -exec dos2unix {} \;
 
 	# Ensure freetype is configured first
-	$mingw32 bash -c "cd src/extlib/src/freetype-2.3.5 && CFLAGS='-c -Wno-error=incompatible-pointer-types' ./configure --enable-static --disable-shared"
+	$mingw32 bash -c "cd src/extlib/src/freetype-2.3.5 && CFLAGS='-Wno-error=incompatible-pointer-types' ./configure --enable-static --disable-shared"
 
 	# Try to force freetype build to not try to make any executables (compile it first) by adding '-c' flag
 	if $mingw32 make -C src/extlib/src/freetype-2.3.5 CFLAGS="-c -Wno-error=incompatible-pointer-types"; then
